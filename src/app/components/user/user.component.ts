b85fd0a732aac834bb8a287cba9b879ac4d24fe4
govardhanreddy <govardhanreddy227@gmail.com>
2019-12-24 15:32:50 +0530

    remove spaces

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { CurdserviceService } from '../../service/curdservice.service';
-import { Hero } from '../../models/hero';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
+import { switchMap } from "rxjs/operators";
+import { CurdserviceService } from "../../service/curdservice.service";
+import { Hero } from "../../models/hero";
 declare var $: any;
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"]
 })
 export class UserComponent implements OnInit {
   username: string;
   password: string;
   userData: any;
-  name = '';
-  value:string;
+  name = "";
+  value: string;
   public heroes: Hero[];
   private selectedData: any;
-  constructor(private route: ActivatedRoute, private curdserviceService: CurdserviceService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private curdserviceService: CurdserviceService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -28,10 +31,9 @@ export class UserComponent implements OnInit {
   }
 
   getUsers() {
-    this.curdserviceService.getHeroes().subscribe(
-      data => {
-        this.heroes = data
-      });
+    this.curdserviceService.getHeroes().subscribe(data => {
+      this.heroes = data;
+    });
   }
 
   // getSelectedUser(id) {
@@ -41,43 +43,39 @@ export class UserComponent implements OnInit {
   //     }
   //   )}
 
-
-  updateUser(id,value) {
+  updateUser(id, value) {
     this.value = value;
     this.curdserviceService.getSelecetedData(id).subscribe(data => {
       this.userData = data;
-      $('#exampleModalCenter').modal('show');
+      $("#exampleModalCenter").modal("show");
       this.name = this.userData.name;
     });
   }
   addUser() {
     let user = {
-      'id': this.heroes.length + 1,
-      'name': this.name
-    }
-    if(this.value == 'update'){
-    this.userData.name = this.name;
+      id: this.heroes.length + 1,
+      name: this.name
+    };
+    if (this.value == "update") {
+      this.userData.name = this.name;
       this.curdserviceService.updateUser(this.userData).subscribe(data1 => {
         this.name = "";
         this.value = null;
-        $('#exampleModalCenter').modal('hide');
+        $("#exampleModalCenter").modal("hide");
       });
-    }else{
-      this.curdserviceService.addUser(user).subscribe(
-        data => {
-          this.name = "";
-          $('#exampleModalCenter').modal('hide');
-        }
-      );
+    } else {
+      this.curdserviceService.addUser(user).subscribe(data => {
+        this.name = "";
+        $("#exampleModalCenter").modal("hide");
+      });
     }
-    
+
     this.getUsers();
   }
 
   deleteUser(id) {
     this.curdserviceService.deleteUser(id).subscribe(data => {
-       this.getUsers();
+      this.getUsers();
     });
   }
-
 }
